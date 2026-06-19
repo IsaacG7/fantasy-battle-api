731b7be86f9e4fa3b9cf7066e618e0a5fc0e5c3d
isaac godsey <idgodsey23@gmail.com>
2026-06-18 22:25:04 -0500

    finishing up character storage

--- FantasyBattleAPI/Models/Character.cs
+++ FantasyBattleAPI/Models/Character.cs
@@ -6,9 +6,9 @@
         public int Level { get; set; }
         public int Attack { get; set; }
         public int Defense { get; set; }
+        public int Hp {  get; set; }
         public string Name { get; set; }
+        public string Race { get; set; }
         public string CharacterClass { get; set; }
-
-
     }
 }
---
+++ FantasyBattleAPI/Services/CharacterStore.cs
@@ -0,0 +1,53 @@
+﻿using FantasyBattleAPI.Models;
+
+namespace FantasyBattleAPI.Services
+{
+    public class CharacterStore
+    {
+        private List<Character> _characters = new List<Character>();
+
+        public bool AddCharacter(Character c)
+        {
+            foreach (var character in _characters)
+            {
+                if (character.Id == c.Id)
+                {
+                    return false;
+                }
+            }
+            _characters.Add(c);
+            return true;
+        }
+
+        public bool RemoveCharacter(int id)
+        {
+            foreach (var character in _characters)
+            {
+                if (character.Id == id)
+                {
+                    _characters.Remove(character);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public List<Character> GetAllCharacters()
+        {
+            List<Character> clist = new List<Character>(_characters);
+            return clist;
+        }
+
+        public Character GetCharacterById(int id)
+        {
+            foreach (var character in _characters)
+            {
+                if (character.Id == id)
+                {
+                    return character;
+                }
+            }
+            return null;
+        }
+    }
+}
