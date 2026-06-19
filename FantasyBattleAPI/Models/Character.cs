75044adf9890198d12774e318124698ddb9db8b2
isaac godsey <idgodsey23@gmail.com>
2026-06-18 21:02:42 -0500

    Setup solution structure and added character class

---
+++ FantasyBattleAPI/Models/Character.cs
@@ -0,0 +1,14 @@
+﻿namespace FantasyBattleAPI.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public int Level { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public string Name { get; set; }
+        public string CharacterClass { get; set; }
+
+
+    }
+}
