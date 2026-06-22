ebb4fdb7e49c65c88cffb48d25bd6cac56db8996
isaac godsey <idgodsey23@gmail.com>
2026-06-21 23:45:03 -0500

    started adding characters controller

---
+++ FantasyBattleAPI/Controllers/CharactersController.cs
@@ -0,0 +1,17 @@
+﻿using FantasyBattleAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FantasyBattleAPI.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class CharactersController : ControllerBase
+    {
+        private CharacterStore _charactersStore;
+
+        public CharactersController(CharacterStore cs) {
+            _charactersStore = cs;
+
+        }
+    }
+}
