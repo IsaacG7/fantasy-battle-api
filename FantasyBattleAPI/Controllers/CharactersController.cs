6a1b6901d6effe35c9a8bc22bac5b43dee60ea85
isaac godsey <idgodsey23@gmail.com>
2026-07-01 21:54:08 -0500

    Set up post(Create character) and get character by id methods in CharactersController

--- FantasyBattleAPI/Controllers/CharactersController.cs
+++ FantasyBattleAPI/Controllers/CharactersController.cs
@@ -5,22 +5,58 @@ using System.Text.Json;
 
 namespace FantasyBattleAPI.Controllers
 {
+    // This says this will be an api controller which gives us some 
+    // "Baked in" capabilities like automatic model validation
     [ApiController]
-    [Route("[Controller]")]
+
+    // this says any requests starting with api/Characters will be handled by this controller
+    [Route("api/[Controller]")]
     public class CharactersController : ControllerBase
     {
-        private CharacterStore _charactersStore;
+        private CharacterStore _characterStore;
 
+        // Because we registered CharacterStore as a singleton (builder.Services.AddSingleton<CharacterStore>();)
+        // When the framework constructs this controller it looks for the CharacterStore
+        // in the DI container which provides the singleton object and it is injected through cs
         public CharactersController(CharacterStore cs)
         {
-            _charactersStore = cs;
+            _characterStore = cs;
 
         }
-        [HttpGet(Name = "GetCharacterList")]
-        public ActionResult<List<Character>> Get()
-        {
 
-            return Ok(_charactersStore.GetAllCharacters());
+        [HttpGet(Name = "GetCharacterList")]
+        // ActionResult allows us to return the list of characters in JSON
+        // Or error codes
+        public ActionResult<List<Character>> GetAll()
+        {
+            return Ok(_characterStore.GetAllCharacters());
+        }
+
+        [HttpGet("{id}", Name = "GetCharacterByID")]
+        public ActionResult<Character> GetByID(int id)
+        {
+            var character = _characterStore.GetCharacterById(id);
+
+            if (character != null)
+            {
+                return Ok(character);
+
+            }
+            return NotFound();
+        }
+
+
+        
+        [HttpPost(Name ="CreateCharacter")]
+        public ActionResult<Character> CreateCharacter(Character ch)
+        {
+            if(_characterStore.AddCharacter(ch)){
+
+                // Returns 201(new resource created) with a Location header pointing to the new character's GET endpoint,
+                // and the newly created character in the response body.
+                return CreatedAtAction("GetByID", new {id = ch.Id}, ch);
+            }
+            return BadRequest($"Duplicate ID, Could not create character with ID {ch.Id}");
 
         }
     }
